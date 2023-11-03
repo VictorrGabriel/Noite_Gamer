83dad2802725341181012a376ac30cc0fcccfb65
JokerJeans <vg154476@gmail.com>
2023-11-02 23:28:35 -0300

    new update

--- script/script.js
+++ script/script.js
@@ -1,1 +1,23 @@
 alert('Este site ainda está em estapa de desenvolvimento, estamos apenas testando, se quisar pode nos ajudar reportando erros. Obrigado!')
+
+let posts = [document.getElementById ('post1'), document.getElementById ('post2'), document.getElementById ('post3') ]
+let imgPost = [document.getElementById ('img_post_1'), document.getElementById ('img_post_2'), document.getElementById ('img_post_3')]
+ 
+
+/* window.onload = function() {
+    let ArtigoPrincipal = [document.createElement ('img'), document.getElementById ('principal')]
+    ArtigoPrincipal[0].setAttribute ('id', 'imgPrincipal' )
+    ArtigoPrincipal[1].appendChild (ArtigoPrincipal[0])
+    imgPrincipal.setAttribute ('src', '../Reporter Jogo/imagens/banner_290.jpeg' )
+    imgPrincipal.style.width = '200px'
+    
+} */
+
+
+
+
+
+/*let ArtigoPrincipal = [document.createElement ('img')]
+ArtigoPrincipal[0].setAttribute ('id', 'imgPrincipal' )
+imgPrincipal.setAttribute ('src', '../Reporter Jogo/imagens/banner_290.jpeg' )
+imgPrincipal.style.width = '200px'*/
