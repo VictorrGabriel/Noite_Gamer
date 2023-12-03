b37833c384dd447783673ebc9a22e5481c795c1e
JokerJeans <vg154476@gmail.com>
2023-12-03 20:01:50 -0300

    nova atualização

--- index/script/script.js
+++ index/script/script.js
@@ -1,30 +1,11 @@
-/* alert(
+ alert(
   "Este site ainda está em estapa de desenvolvimento, estamos apenas testando, se quisar pode nos ajudar reportando erros. Obrigado!"
-); */
+); 
 
-let posts = [
-  document.getElementById("post1"),
-  document.getElementById("post2"),
-  document.getElementById("post3"),
-];
-let imgPost = [
-  document.getElementById("img_post_1"),
-  document.getElementById("img_post_2"),
-  document.getElementById("img_post_3"),
-];
+/* const mostRecentPost1 = document.getElementById('most_recent_1');
 
-/* window.onload = function() {
-    let ArtigoPrincipal = [document.createElement ('img'), document.getElementById ('principal')]
-    ArtigoPrincipal[0].setAttribute ('id', 'imgPrincipal' )
-    ArtigoPrincipal[1].appendChild (ArtigoPrincipal[0])
-    imgPrincipal.setAttribute ('src', '../Reporter Jogo/imagens/banner_290.jpeg' )
-    imgPrincipal.style.width = '200px'
-    
+function loadFirtsPost(){
+  mostRecentPost1.style.backgroundImage = "url('../p.jpeg')"
+
 } */
-
-/*let ArtigoPrincipal = [document.createElement ('img')]
-ArtigoPrincipal[0].setAttribute ('id', 'imgPrincipal' )
-imgPrincipal.setAttribute ('src', '../Reporter Jogo/imagens/banner_290.jpeg' )
-imgPrincipal.style.width = '200px'*/
-
 
